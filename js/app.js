10d8d14bd15c86ab4f2fce7aff2dc4cb8cba5b74
Roberto <rperez15@ucol.mx>
2022-09-01 00:06:57 -0500

    Version con css 2

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ btnAgregar.addEventListener('click', () => {
   arbol.agregar(prod);
 
   let detalles = document.getElementById('detalles');
-  detalles.innerHTML += '<p>Se agrego producto: ' + nom + '</p>';
+  detalles.innerHTML += '<h3>Se agrego producto: </h3>' + nom;
 })
 
 const btnBuscar = document.getElementById('btnBuscar');
@@ -21,7 +21,7 @@ btnBuscar.addEventListener('click', () =>{
   if (resultado != "")
     detalles.innerHTML += '<h3>Se encontro:</h3>' + resultado ;
   else
-    detalles.innerHTML += '<p>No existe el producto<p>';
+    detalles.innerHTML += '<h2>No existe el producto</h2>';
 })
 
 const btnInorder = document.getElementById('btnInorder');
@@ -29,9 +29,9 @@ btnInorder.addEventListener('click', () => {
   let resultado = arbol.inOrder();
   let detalles = document.getElementById('detalles');
   if (resultado != "")
-    detalles.innerHTML += '<h3>InOrder</h3>' + resultado;
+    detalles.innerHTML += '<h3>Inorder</h3>' + resultado;
   else
-    detalles.innerHTML += '<p>No existen productos</p>'
+    detalles.innerHTML += '<h2>No existen productos</h2>'
 });
 
 const btnPreorder = document.getElementById('btnPreorder');
@@ -39,9 +39,9 @@ btnPreorder.addEventListener('click', () =>{
   let resultado = arbol.preOrder();
   let detalles = document.getElementById('detalles');
   if (resultado != "")
-    detalles.innerHTML += '<h3>PreOrder</h3>' + resultado;
+    detalles.innerHTML += '<h3>Preorder</h3>' + resultado;
   else
-    detalles.innerHTML += '<p>No existen productos</p>'
+    detalles.innerHTML += '<h2>No existen productos</h2>'
 });
 
 const btnPostorder = document.getElementById('btnPostorder');
@@ -49,9 +49,9 @@ btnPostorder.addEventListener('click', () =>{
   let resultado = arbol.postOrder();
   let detalles = document.getElementById('detalles');
   if (resultado != "")
-    detalles.innerHTML += '<h3>PosOrder</h3>' + resultado;
+    detalles.innerHTML += '<h3>Postorder</h3>' + resultado;
   else
-    detalles.innerHTML += '<p>No existen productos</p>'
+    detalles.innerHTML += '<h2>No existen productos</h2>'
 });
 
 const btnClear = document.getElementById('btnClear');
